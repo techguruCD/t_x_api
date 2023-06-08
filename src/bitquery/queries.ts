ad60cdb41f811c3011dc0d31f1da5dbbf1f2d0bf
rahultrivedi180 <rahultrivedi180.eth@ethereum.email>
2023-06-08 16:31:47 +0530

    feat: price subscription query

--- src/bitquery/queries.ts
+++ src/bitquery/queries.ts
@@ -86,7 +86,52 @@ function searchPairsByCurrency(params: {
   return query;
 }
 
+function priceSubscription(params: {
+  network: 'eth' | 'bsc';
+  buyCurrency: string;
+  sellCurrency: string;
+}) {
+  const query = `subscription {
+  EVM(network: ${params.network}, trigger_on: all) {
+    DEXTrades(
+      limit: {count: 1}
+      orderBy: {descending: Block_Number}
+      where: {Trade: {Buy: {Currency: {SmartContract: {is: "${params.buyCurrency}"}}}, Sell: {Currency: {SmartContract: {is: "${params.sellCurrency}"}}}}}
+    ) {
+      Trade {
+        Buy {
+          Amount
+          Price
+          Currency {
+            SmartContract
+            Name
+            Symbol
+          }
+        }
+        Dex {
+          ProtocolFamily
+          SmartContract
+        }
+        Sell {
+          Amount
+          Price
+          Currency {
+            SmartContract
+            Symbol
+            Name
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+  return query;
+}
+
 export default {
   searchTokenByString,
   searchPairsByCurrency,
+  priceSubscription,
 };

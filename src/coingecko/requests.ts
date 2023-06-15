8702af09221fb89d5000691da34462a154b79236
rahultrivedi180 <rahultrivedi180.eth@ethereum.email>
2023-06-15 20:41:07 +0530

    feat: get coin data

--- src/coingecko/requests.ts
+++ src/coingecko/requests.ts
@@ -2,16 +2,14 @@ import { isAxiosError } from 'axios';
 import cgAxios from './cgAxios';
 import { ExpressError } from '../utils/error.utils';
 
-type TStringBoolean = 'true' | 'false';
-
 async function tokenPrice(params: {
   id: string;
   contract_addresses: string[];
   vs_currencies: string[];
-  include_market_cap?: TStringBoolean;
-  include_24hr_vol?: TStringBoolean;
-  include_24hr_change?: TStringBoolean;
-  include_last_updated_at?: TStringBoolean;
+  include_market_cap?: boolean;
+  include_24hr_vol?: boolean;
+  include_24hr_change?: boolean;
+  include_last_updated_at?: boolean;
   precision?: string;
 }) {
   try {
@@ -46,8 +44,49 @@ async function tokenPrice(params: {
   }
 }
 
+async function tokenCurrentData(params: {
+  id: string;
+  localization?: boolean;
+  tickers?: boolean;
+  market_data?: boolean;
+  community_data?: boolean;
+  developer_data?: boolean;
+  sparkline?: boolean;
+}) {
+  try {
+    const url = `/coins/${params.id}`;
+    const queryParams = {
+      localization: params.localization ?? true,
+      tickers: params.tickers ?? true,
+      market_data: params.market_data ?? true,
+      community_data: params.community_data ?? true,
+      developer_data: params.developer_data ?? true,
+      sparkline: params.sparkline ?? false,
+    };
+
+    const cgResponse = await cgAxios.get(url, {
+      params: queryParams,
+    });
+    return cgResponse.data;
+  } catch (error: any) {
+    if (isAxiosError(error) && error.response) {
+      throw new ExpressError(
+        'CGE00003',
+        error.response.data.error,
+        error.response.status
+      );
+    }
+    throw new ExpressError(
+      'CGE00004',
+      error.message ?? 'Something Went Wrong',
+      400
+    );
+  }
+}
+
 const cgRequests = {
   tokenPrice,
+  tokenCurrentData,
 };
 
 export default cgRequests;

7c6ce94aacc8cbd82bcff31b5d3732a3d375d16d
rahultrivedi180 <rahultrivedi180.eth@ethereum.email>
2023-06-15 16:31:18 +0530

    fix: use bitquery endpoint from the env

--- src/bitquery/bitqueryAxios.ts
+++ src/bitquery/bitqueryAxios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import env from '../env';
 
 const bitqueryAxios = axios.create({
-  baseURL: 'https://graphql.bitquery.io',
+  baseURL: env().bitqueryEndpoint,
   headers: {
     'Content-Type': 'application/json',
     'X-API-KEY': env().bitqueryApiKey,

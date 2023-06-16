9d6d88ee09e86e304a1c8c2a1fcdff634ae83dae
rahultrivedi180 <rahultrivedi180.eth@ethereum.email>
2023-06-16 10:31:41 +0530

    feat: added favCoins model

---
+++ src/models/favCoins.model.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const favCoinSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    ref: 'Users',
+  },
+  address: {
+    type: String,
+    required: true,
+    ref: 'Coins',
+  },
+  cgTokenPrice: {
+    type: mongoose.Schema.Types.Mixed,
+  },
+  cgTokenInfo: {
+    type: mongoose.Schema.Types.Mixed,
+  },
+  cgMarketChart: {
+    type: mongoose.Schema.Types.Mixed,
+  },
+  cgMarketData: {
+    type: mongoose.Schema.Types.Mixed,
+  },
+});
+
+const favCoinsModel = mongoose.model('FavCoins', favCoinSchema, 'FavCoins');
+
+export default favCoinsModel;

d7e419f7ca2425bb57a249c57e2ead92c02c27d1
rahultrivedi180 <rahultrivedi180.eth@ethereum.email>
2023-06-26 16:48:34 +0530

    fix: update alert service as per requirement

--- src/services/alert.service.ts
+++ src/services/alert.service.ts
@@ -1,34 +1,53 @@
 import alertModel from '../models/alert.model';
+import coinsModel from '../models/coins.model';
 import { ExpressError } from '../utils/error.utils';
 
 async function setAlert(params: {
   userId: string;
-  baseCurrency: string;
-  quoteCurrency?: string;
-  price: number;
-  side?: 'up' | 'down';
+  alertBaseCurrency: string;
+  alertSide: 'up' | 'down';
+  alertPrice?: number;
+  alertPercentage?: number;
 }) {
-  const filterObject: Record<string, string | number> = {
+  const newAlertData: Record<string, any> = {
     userId: params.userId,
-    baseCurrency: params.baseCurrency,
-    price: params.price,
+    alertBaseCurrency: params.alertBaseCurrency,
+    alertSide: params.alertSide,
   };
 
-  if (params.quoteCurrency) {
-    filterObject['quoteCurrency'] = params.quoteCurrency;
+  if (params.alertPrice) {
+    if (params.alertPrice < 0) {
+      throw new ExpressError('AS00001', 'Alert cannot be negative', 400);
+    }
+    newAlertData['alertPrice'] = params.alertPrice;
   }
 
-  if (params.side) {
-    filterObject['side'] = params.side;
+  if (params.alertPercentage) {
+    if (params.alertPercentage > 100 || params.alertPercentage < 0) {
+      throw new ExpressError(
+        'AS00002',
+        'Percentage should be between 0 & 100',
+        400
+      );
+    }
+
+    const coin = await coinsModel
+      .findOne({ address: params.alertBaseCurrency })
+      .lean();
+    if (coin) {
+      newAlertData['alertPercentage'] = params.alertPercentage;
+      const priceInDb = coin.cgTokenInfo.market_data.current_price.usd;
+      if (params.alertSide === 'up') {
+        newAlertData['alertPrice'] =
+          priceInDb + priceInDb * (params.alertPercentage / 100);
+      } else {
+        newAlertData['alertPrice'] =
+          priceInDb - priceInDb * (params.alertPercentage / 100);
+      }
+    }
   }
 
-  const alert = await alertModel.findOne(filterObject).lean();
-
-  if (alert) {
-    throw new ExpressError('SAS00001', 'Alert already set', 400);
-  }
-
-  const newAlert = await new alertModel(filterObject).save();
+  const newAlert = await new alertModel(newAlertData).save();
   return newAlert.toObject();
 }
 

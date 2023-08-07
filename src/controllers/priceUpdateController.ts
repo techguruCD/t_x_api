828a44c3c4467614ee58e2ddfb4298a37f300ffb
rahultrivedi180 <rahultrivedi180.eth@ethereum.email>
2023-08-07 14:11:53 +0530

    fix: stop priceUpdateController

--- src/controllers/priceUpdateController.ts
+++ src/controllers/priceUpdateController.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
-import favCoinsModel from '../models/favCoins.model';
-import cgRequests from '../coingecko/requests';
-import coinsModel from '../models/coins.model';
+// import favCoinsModel from '../models/favCoins.model';
+// import cgRequests from '../coingecko/requests';
+// import coinsModel from '../models/coins.model';
 
 async function priceUpdateController(
   _req: Request,
@@ -9,55 +9,55 @@ async function priceUpdateController(
   _next: NextFunction
 ) {
   try {
-    const coinGroups = await favCoinsModel.aggregate([
-      {
-        $lookup: {
-          from: 'Coins',
-          localField: 'address',
-          foreignField: 'address',
-          as: 'coinInfo',
-        },
-      },
-      {
-        $unwind: {
-          path: '$coinInfo',
-          preserveNullAndEmptyArrays: true,
-        },
-      },
-      {
-        $group: {
-          _id: null,
-          ids: {
-            $addToSet: "$coinInfo.cgMarketData.id",
-          },
-        }
-      },
-    ]);
+    // const coinGroups = await favCoinsModel.aggregate([
+    //   {
+    //     $lookup: {
+    //       from: 'Coins',
+    //       localField: 'address',
+    //       foreignField: 'address',
+    //       as: 'coinInfo',
+    //     },
+    //   },
+    //   {
+    //     $unwind: {
+    //       path: '$coinInfo',
+    //       preserveNullAndEmptyArrays: true,
+    //     },
+    //   },
+    //   {
+    //     $group: {
+    //       _id: null,
+    //       ids: {
+    //         $addToSet: "$coinInfo.cgMarketData.id",
+    //       },
+    //     }
+    //   },
+    // ]);
 
-    if (coinGroups.length < 1) {
-      return _res.status(200).json({ success: true });
-    }
+    // if (coinGroups.length < 1) {
+    //   return _res.status(200).json({ success: true });
+    // }
 
-    const ids = coinGroups[0].ids ?? [];
+    // const ids = coinGroups[0].ids ?? [];
 
-    const cgMarketData = await cgRequests.coinMarketData({ ids });
+    // const cgMarketData = await cgRequests.coinMarketData({ ids });
 
-    const cgMarketUpdates = [];
+    // const cgMarketUpdates = [];
 
-    for (let i = 0; i < cgMarketData.length; i++) {
-      const id = cgMarketData[i].id;
-      cgMarketUpdates.push({
-        updateOne: {
-          filter: { "cgMarketData.id": id },
-          update: { $set: { "cgMarketData": cgMarketData[i] } },
-          upsert: true
-        }
-      })
-    }
+    // for (let i = 0; i < cgMarketData.length; i++) {
+    //   const id = cgMarketData[i].id;
+    //   cgMarketUpdates.push({
+    //     updateOne: {
+    //       filter: { "cgMarketData.id": id },
+    //       update: { $set: { "cgMarketData": cgMarketData[i] } },
+    //       upsert: true
+    //     }
+    //   })
+    // }
 
-    if (cgMarketUpdates.length > 0) {
-      await coinsModel.bulkWrite(cgMarketUpdates);
-    }
+    // if (cgMarketUpdates.length > 0) {
+    //   await coinsModel.bulkWrite(cgMarketUpdates);
+    // }
 
     return _res.status(200).json({ success: true });
   } catch (error) {
